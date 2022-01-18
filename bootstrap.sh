ec07a44c5db59f89d73f3d92fee5fb6f9bf3b957
Avishkar Autar <aautar@digital-radiation.com>
2022-01-17 19:36:36 -0500

    install git, update provision playbook to pull latest from raspberry-pi-ansible-bootstrap repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,11 +22,11 @@ sudo chown -R ansible_user /home/ansible_user/.ssh
 # Add ansible_user to sudoers, no password required for sudo elevation
 echo "ansible_user  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible_user
 
-# Create folder with provisioning content
-cd /raspberry-pi-ansible-bootstrap
-sudo mkdir /provision
-sudo cp provision.yml /provision/provision.yml
-sudo cp app.js /provision/app.js
+# Install Git
+sudo apt install git
+
+# Clone repo with bootstrap
+sudo git clone https://github.com/aautar/raspberry-pi-ansible-bootstrap.git /home/ansible_user/
 
 # Enable logging for Ansible
 sudo touch /var/log/ansible.log
@@ -34,12 +34,12 @@ sudo setfacl -m u:ansible_user:rwx /var/log/ansible.log
 sudo sed -i 's|#log_path = /var/log/ansible.log|log_path = /var/log/ansible.log|' /etc/ansible/ansible.cfg
 
 # Create ansible inventory
-sudo touch /provision/inventory.ini
-sudo cat << 'ENDINV' > /provision/inventory.ini
+sudo touch /home/ansible_user/inventory.ini
+sudo cat << 'ENDINV' > /home/ansible_user/inventory.ini
 [hosts]
 localhost ansible_connection=ssh ansible_user=ansible_user
 ENDINV
 
 # Run ansible playbook
 export PYTHONUNBUFFERED=1
-sudo -u ansible_user ansible-playbook /provision/provision.yml -i /provision/inventory.ini
+sudo -u ansible_user ansible-playbook /home/ansible_user/raspberry-pi-ansible-bootstrap/provision.yml -i /home/ansible_user/inventory.ini
