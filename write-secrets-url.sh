b34563d7b03485842da8d23c62773ee866fca716
Avishkar Autar <aautar@digital-radiation.com>
2022-01-29 18:38:59 -0500

    tweak write-secrets-url script to touch app-secrets-url.txt

--- write-secrets-url.sh
+++ write-secrets-url.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Writing secrets URL file"
+sudo -u ansible_user touch /home/ansible_user/.app/app-secrets-url.txt
 echo "$1" | sudo -u ansible_user tee /home/ansible_user/.app/app-secrets-url.txt > /dev/null
