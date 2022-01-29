ee33cd4fa55d679431e5eaffb407c6ff5cbd1a51
Avishkar Autar <aautar@digital-radiation.com>
2022-01-29 18:40:23 -0500

    add step to make .app subdir in write-secrets-url script

--- write-secrets-url.sh
+++ write-secrets-url.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Writing secrets URL file"
+sudo -u ansible_user mkdir /home/ansible_user/.app/
 sudo -u ansible_user touch /home/ansible_user/.app/app-secrets-url.txt
 echo "$1" | sudo -u ansible_user tee /home/ansible_user/.app/app-secrets-url.txt > /dev/null
