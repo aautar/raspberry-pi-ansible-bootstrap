56f62b7ae51393990be6bc988dd8824e6fccf3b8
Avishkar Autar <aautar@digital-radiation.com>
2022-01-29 19:07:17 -0500

    correct handling of password for app secret URL target

--- write-secrets-url.sh
+++ write-secrets-url.sh
@@ -4,3 +4,6 @@ echo "Writing secrets URL file"
 sudo -u ansible_user mkdir -p /home/ansible_user/.app/
 sudo -u ansible_user touch /home/ansible_user/.app/app-secrets-url.txt
 echo "$1" | sudo -u ansible_user tee /home/ansible_user/.app/app-secrets-url.txt > /dev/null
+
+read -p "Enter password for secrets URL target: " -s pw
+echo "$pw" | sudo -u ansible_user tee /home/ansible_user/.app/app-secrets-remote-password.txt > /dev/null
